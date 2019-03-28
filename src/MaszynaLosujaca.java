ff950f330bafd3c460be03edbddba7709824ea24
Gabriel Szwaczkiewicz <gabriel.szwaczkiewicz@gmail.com>
2019-03-28 21:36:27 +0100

    Working roulette

--- src/MaszynaLosujaca.java
+++ src/MaszynaLosujaca.java
@@ -26,4 +26,43 @@ public class MaszynaLosujaca {
 
         return osobniki;
     }
+    public ArrayList<Osobnik> ruletka(ArrayList<Osobnik> osobniki){
+        ArrayList<Osobnik> ret = new ArrayList<>();
+        int pop = osobniki.size();
+        double suma_fit = 0;
+        double min_fit = osobniki.get(0).benefit;
+        for(Osobnik os: osobniki){
+            if(min_fit > os.benefit){
+                min_fit = os.benefit;
+            }
+            suma_fit += os.benefit;
+        }
+        if(min_fit < 0){
+            suma_fit -= min_fit * pop;
+        }
+        else
+            min_fit = 0;
+
+        for(int i = 0; i < pop; i++){
+            double krecenie = Math.random() * suma_fit;
+            double pol = 0;
+            boolean stop = false;
+            for(int j =0; j < pop && !stop; j++){
+                pol += osobniki.get(j).benefit - min_fit;
+                if(pol > krecenie){
+                    stop = true;
+                    ret.add(osobniki.get(j));
+                    System.out.println(min_fit + ", " + osobniki.get(j).benefit);
+                }
+            }
+        }
+        System.out.println("Populacja: "+  ret.size());
+
+
+        return ret;
+    }
+    public ArrayList<Osobnik> turniej(ArrayList<Osobnik> osobniki){
+        return osobniki;
+    }
+
 }
