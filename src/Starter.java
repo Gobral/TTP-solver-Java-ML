65a8528018088ba16fb757f4a466196fd6bdb948
Gabriel Szwaczkiewicz <gabriel.szwaczkiewicz@gmail.com>
2019-04-12 02:10:02 +0200

    Added some code to generate better statistics. Added examples of statistics in csv

--- src/Starter.java
+++ src/Starter.java
@@ -7,12 +7,14 @@ import java.util.Scanner;
 public class Starter {
     public static void main(String args[]){
 
+        long czasRozpoczecia = System.currentTimeMillis();
+
         Scanner sc= new Scanner(System.in);
         int pop_size = 100;
-        int gen = 350;
-        double px= 0.7;
-        double pm = 0.01 ;
-        int tour = 5;
+        int gen = 100;
+        double px = 0.80;
+        double pm = 0.07;
+        int tour = 15;
         int ilosc_ruchomien = 20;
         /*
         pop_size = sc.nextInt();
@@ -29,24 +31,31 @@ public class Starter {
 
         FileWriter fw = null;
         try {
-            fw = new FileWriter("statystyka350_rul.csv");
+            fw = new FileWriter("statystyka_tur_15.csv");
             PrintWriter printWriter = new PrintWriter(fw);
             printWriter.println("SEP=,");
             double[] usrednione = new double[gen];
+            //double[] maksymalne = new double[gen];
+            //double[] minimalne = new double[gen];
             for(int g = 0; g < gen; g++){
                 usrednione[g] = 0;
+                //maksymalne[g] = 0;
+                //minimalne[g] = 0;
             }
             for(int i = 0; i < ilosc_ruchomien; i++){
                 Genetyk algorytm_genetyczny = new Genetyk(loader, ttp, ksp, pop_size, gen, px, pm, tour);
                 double[][] wynik = algorytm_genetyczny.get_generations();
                 for(int g = 0; g < gen; g++){
                     usrednione[g] += wynik[g][1] / ilosc_ruchomien;
+                    //maksymalne[g] += wynik[g][0] / ilosc_ruchomien;
+                    //minimalne[g] += wynik[g][2] / ilosc_ruchomien;
                 }
             }
             //double[][] wynik = algorytm_genetyczny.get_generations();
             for(int g = 0; g < gen; g++){
                 //System.out.println(g);
                 //printWriter.println(g + ", " + wynik[g][0] + ", " + wynik[g][1] + ", " + wynik[g][2]);
+                //printWriter.println(g + ", " + maksymalne[g] + ", "+ usrednione[g] + ", "  + minimalne[g]);
                 printWriter.println(g + ", " + usrednione[g]);
             }
 
@@ -54,7 +63,8 @@ public class Starter {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
+        long czasZakonczenia = System.currentTimeMillis();
+        long czasTrwania = czasZakonczenia - czasRozpoczecia;
+        System.out.println("Czas trwania: " + czasTrwania);
     }
 }
